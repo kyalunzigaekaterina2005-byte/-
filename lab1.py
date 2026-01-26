3e27687bc1b89c07811217418ab77dd8b5fbbb60
kyalunzigaekaterina2005-byte <kyalunzigaekaterina2005@gmail.com>
2026-01-26 14:08:43 +1000

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,31 +1,26 @@
 import random
 
-def grandma():
-    print("ЧЕГО СКАЗАТЬ-ТО ХОТЕЛ, МИЛОК?!")
-    
-    bye = 0
-    while True:
-        user_answer = input("> ")
+bye = 0
 
-        if user_answer == "ПОКА!":
-            bye += 1
-            
-            if bye == 3:
-                break
-            
+print("ЧЕГО СКАЗАТЬ-ТО ХОТЕЛ, МИЛОК?!")
+
+while bye < 3:
+    user_answer = input("> ")
+
+    if user_answer == "ПОКА!":
+        bye += 1
+        
+        if bye < 3:
             year = random.randint(1930, 1950)
             print(f"НЕТ, НИ РАЗУ С {year} ГОДА!")
             
-        elif user_answer.isupper() and user_answer != "":
-            bye = 0
-            year = random.randint(1930, 1950)
-            print(f"НЕТ, НИ РАЗУ С {year} ГОДА!")
-            
-        else:
-            bye = 0
-            print("АСЬ?! ГОВОРИ ГРОМЧЕ, ВНУЧЕК!")
+    elif user_answer.isupper() and user_answer != "":
+        bye = 0
+        year = random.randint(1930, 1950)
+        print(f"НЕТ, НИ РАЗУ С {year} ГОДА!")
+        
+    else:
+        bye = 0  
+        print("АСЬ?! ГОВОРИ ГРОМЧЕ, ВНУЧЕК!")
 
-    print("ДО СВИДАНИЯ, МИЛЫЙ!")
-
-if __name__ == "__main__":
-    grandma()
+print("ДО СВИДАНИЯ, МИЛЫЙ!")
